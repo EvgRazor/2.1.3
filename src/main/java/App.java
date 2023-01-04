a76d5b62a7af8a7e68a58e09b8c71c500e82772b
Evgeni Gud <goodevgeni@gmail.com>
2023-01-04 13:05:21 +0300

    2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,25 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        // по умолчанию отрабатывает Singleton. Ссылки ведут на один объект
+        System.out.println(bean == bean1);
         System.out.println(bean.getMessage());
+
+
+
+        // поменяли с Singleton на Prototype
+        Cat cat =  applicationContext.getBean(Cat.class);
+        Cat cat2 =  applicationContext.getBean(Cat.class);
+
+        System.out.println(cat == cat2);
+
+
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String name;
+
+    public String getMessage() {
+        return name;
+    }
+
+    public void setMessage(String message) {
+        this.name = message;
+    }
+}
